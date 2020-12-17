c459b1e0d0b097b474791e61d6c1a6f8ab500380
Gibson05 <68709697+Gibson05@users.noreply.github.com>
2020-12-17 10:56:15 +0700

    fix bt lan 5

--- JS Basic Home Work/homework7.js
+++ JS Basic Home Work/homework7.js
@@ -28,7 +28,7 @@ function divideSum(a) {
     var sum = a;
     var b = a / 2;
     for (i = 0; i <= b; i++) {
-        if (b % i == 0) {
+        if (a % i == 0) {
             sum += i;
         }
     }
